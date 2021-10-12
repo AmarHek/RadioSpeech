8712e7911c5062f091e503820416553be846615e
amh12ry <amar.hekalo@uni-wuerzburg.de>
2021-10-12 12:35:08 +0200

    update server port to 8000

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backend: "http://localhost:8000/",
+  backend: "./",
   authentication: "radio/authentication/this/is/very/safe/133742069/",
   database: "radio/database/",
   template: "template/",
