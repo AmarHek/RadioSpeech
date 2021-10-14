aff92b835796f2eb70887c9c2a6a4d7e73009206
amh12ry <amar.hekalo@uni-wuerzburg.de>
2021-10-14 18:01:20 +0200

    added origin

--- backend/src/config/cors.config.ts
+++ backend/src/config/cors.config.ts
@@ -1,3 +1,4 @@
 export const corsOptions = {
-    origin: "http://localhost:4200"
+    origin: ["http://localhost:4200", "https://radiospeech.informatik.uni-wuerzburg.de"],
+    default: "http://localhost:4200"
 };
