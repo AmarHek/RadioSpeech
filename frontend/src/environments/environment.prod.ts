24818024c17de579c9d5dcf952e8202292d62d99
amh12ry <amar.hekalo@uni-wuerzburg.de>
2021-10-15 14:22:51 +0200

    update environment prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   backend: "https://radiospeech.informatik.uni-wuerzburg.de/",
   authentication: "radio/authentication/this/is/very/safe/133742069/",
   database: "radio/database/",
