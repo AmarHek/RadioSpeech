0b3ead53db8d316423b36bb131f66e5652aa283b
Amar Hekalo <amar.hekalo@uni-wuerzburg.de>
2022-02-15 15:15:54 +0100

    updated paths in tsconfig, added init.ts

---
+++ backend/src/init.ts
@@ -0,0 +1,28 @@
+import * as Path from "path";
+import * as fs from "fs";
+import * as mongoose from "mongoose";
+import Template from './models/template.schema';
+import * as TemplateController from "src/co"
+
+// on app start, initialize all relative directories for file saving etc.
+export function initDirectories() {
+    if (!fs.existsSync(Path.join(process.cwd(), "data"))) {
+        fs.mkdirSync(Path.join(process.cwd(), "data"));
+    }
+    if (!fs.existsSync(Path.join(process.cwd(), "data/images"))) {
+        fs.mkdirSync(Path.join(process.cwd(), "data/images"));
+    }
+}
+
+export function loadDefaultTemplates() {
+    if (!fs.existsSync(Path.join(process.cwd(), "src/assets/Radiolearn.json"))) {
+        console.warn("Radiolearn.json asset missing!")
+    } else {
+        Template.countDocuments({name: "Radiolearn"}, (err, count) => {
+            if (count === 0) {
+                console.log("Radiolearn missing from database. Adding entry from assets.");
+
+            }
+        })
+    }
+}
