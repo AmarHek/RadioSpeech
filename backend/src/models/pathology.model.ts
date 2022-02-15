57c55bb4c6ffab975dcb0193afa822639a7dde49
Amar Hekalo <amar.hekalo@uni-wuerzburg.de>
2022-02-15 17:21:46 +0100

    added pathology model and started asset json-file for mongo

---
+++ backend/src/models/pathology.model.ts
@@ -0,0 +1,20 @@
+export interface Pathology {
+    name: string;
+    englishName: string;
+    templateMaps: TemplateMap[];
+}
+
+export type TemplateMap = CheckBoxMap | OptionMap;
+
+interface CheckBoxMap {
+    categoryName: string;
+    kind: "box";
+    name: string;
+}
+
+interface OptionMap {
+    categoryName: string;
+    kind: "option";
+    groupName: string;
+    name: string;
+}
