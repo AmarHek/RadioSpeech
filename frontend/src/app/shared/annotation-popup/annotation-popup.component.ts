fc8a02185abf75db2cb98aea84da46ee7481d9a9
Micha Nowak <gonderjake@gmail.com>
2023-05-16 23:50:26 +0200

    add variable support to box popup

--- frontend/src/app/shared/annotation-popup/annotation-popup.component.ts
+++ frontend/src/app/shared/annotation-popup/annotation-popup.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {Category} from "@app/models";
+import {CheckBox, Selectable} from "@app/models";
 
 @Component({
   selector: 'app-annotation-popup',
@@ -9,22 +9,56 @@ import {Category} from "@app/models";
 })
 export class AnnotationPopupComponent implements OnInit {
 
-  categories: Category[];
+  pathologies: Selectable[];
+  selectedPathology: string = "";
+  options: string[] = [];
+  title = "Diagnose"
+
   constructor(
     private dialogRef: MatDialogRef<AnnotationPopupComponent>,
     @Inject(MAT_DIALOG_DATA) public data
-  ) { }
-
-  ngOnInit(): void {
-    this.categories = this.data.categories
+  ) {
   }
 
-  close(){
+  ngOnInit(): void {
+    this.pathologies = this.data.categories[0].selectables
+    this.pathologies.forEach(selectable => {
+      this.options.push(selectable.name)
+    })
+  }
+
+  close() {
     this.dialogRef.close(false)
   }
 
-  annotationSelected(name){
-    this.dialogRef.close(name)
+  annotationSelected(name) {
+    this.options = []
+    console.log(this.pathologies)
+    if (this.selectedPathology == "") {
+      this.selectedPathology = name
+      this.title = name
+      let variablesExist = false
+      this.pathologies.forEach(pathology => {
+        pathology = pathology as CheckBox
+        if (pathology.name == this.selectedPathology && pathology.variables.length > 0){
+          variablesExist = true
+          if (pathology.variables[0].kind == "oc"){
+            pathology.variables[0].values.forEach(value => {
+              this.options.push(value)
+            })
+          } else if (pathology.variables[0].kind == "mc"){
+            pathology.variables[0].values.forEach(value => {
+              this.options.push(value[0])
+            })
+          }
+        }
+      })
+      if(!variablesExist){
+        this.dialogRef.close(name)
+      }
+    } else {
+      this.dialogRef.close(this.selectedPathology + " | " + name)
+    }
   }
 
 }
