bd7d5cf6b875d3f382dd02b98f8ea5ebdc10ec55
Micha Nowak <gonderjake@gmail.com>
2023-06-13 06:45:01 +0200

    implement setting persistence for dialog

--- frontend/src/app/shared/settings-dialog/settings-dialog.component.ts
+++ frontend/src/app/shared/settings-dialog/settings-dialog.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
+import {SettingsService} from "@app/core/services/settings.service";
 
 @Component({
   selector: 'app-settings-dialog',
@@ -10,16 +11,29 @@ export class SettingsDialogComponent implements OnInit {
 
   color_mode?: string = "standard"
   language?: string = "german"
-  constructor(private dialogRef: MatDialogRef<SettingsDialogComponent>) { }
 
-  close_dialog(){
+  constructor(private dialogRef: MatDialogRef<SettingsDialogComponent>,
+              private settingsService: SettingsService) {
+  }
+
+  ngOnInit(): void {
+    this.load_settings()
+  }
+
+  load_settings() {
+    this.color_mode = this.settingsService.getSetting(this.settingsService.Settings.ColorTheme.ID)
+    this.language = this.settingsService.getSetting(this.settingsService.Settings.Language.ID)
+  }
+
+  save_settings() {
+    this.settingsService.setSetting(this.settingsService.Settings.ColorTheme.ID, this.color_mode)
+    this.settingsService.setSetting(this.settingsService.Settings.Language.ID, this.language)
+    this.close_dialog()
+  }
+
+  close_dialog() {
     this.dialogRef.close()
   }
 
-  save_settings(){
-    this.close_dialog()
-  }
-  ngOnInit(): void {
-  }
 
 }
