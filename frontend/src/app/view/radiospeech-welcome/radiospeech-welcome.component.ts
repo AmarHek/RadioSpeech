75c9ba98f58ac373c313ab2f7b74600760ede75f
Micha Nowak <gonderjake@gmail.com>
2023-11-01 22:20:06 +0100

    remove unused code, welcome component

--- frontend/src/app/view/radiospeech-welcome/radiospeech-welcome.component.ts
+++ frontend/src/app/view/radiospeech-welcome/radiospeech-welcome.component.ts
@@ -1,7 +1,4 @@
 import {Component, OnInit} from "@angular/core";
-import {Router} from "@angular/router";
-import {environment} from "@env/environment";
-import {getUUID} from "@app/helpers/localStorageHelper";
 
 @Component({
   selector: "app-radiospeech-welcome",
@@ -10,18 +7,9 @@ import {getUUID} from "@app/helpers/localStorageHelper";
 })
 export class RadiospeechWelcomeComponent implements OnInit {
 
-  assetsUrl = environment.assets;
-
-  private UUID = "undefined";
-
-  constructor(private router: Router) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.UUID = getUUID();
-  }
-
-  openEditor(matID: string) {
-    this.router.navigate(["/", "radiolearn", "main", matID]).then();
   }
 }
